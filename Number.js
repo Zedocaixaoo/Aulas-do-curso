f1bdec6a78a56a0bab53f46885388e1f26a29571
Zedocaixaoo <160745761+Zedocaixaoo@users.noreply.github.com>
2024-03-13 09:11:11 -0300

    Add files via upload

---
+++ Number.js
@@ -0,0 +1,17 @@
+// código omitido
+
+// ponto flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuanteSemZero = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+// código omitido
+
+// NaN -> Not A Number (não é um número)
+
+const alura = "Alura";
+console.log(alura * primeiroNumero)
