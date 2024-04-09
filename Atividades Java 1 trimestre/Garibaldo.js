843c9651e7bb09257c0ef79f49b8cac395cabf69
Zedocaixaoo <160745761+Zedocaixaoo@users.noreply.github.com>
2024-04-09 10:12:47 -0300

    Add files via upload

---
+++ Atividades Java 1 trimestre/Garibaldo.js
@@ -0,0 +1,9 @@
+const nome = "Garibaldo";
+const idade = 2024-1972;
+const cidadeDeNascimento = "Nova York";
+
+// const apresentacao = "meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = "meu nome é ${Garibaldo}, minha idade é ${54} e nasci na cidade de ${Nova York}";
+
+console.log(apresentacao)
